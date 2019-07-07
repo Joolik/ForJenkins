f5e8f046ced39e9f099157f061355049c273f6d3
Joolik <yiulaz@yandex.ru>
2019-07-07 23:22:45 +0300

    initial

--- src/test/java/y/z/BaseTest.java
+++ src/test/java/y/z/BaseTest.java
@@ -3,6 +3,7 @@ package y.z;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -16,11 +17,14 @@ import java.util.concurrent.TimeUnit;
 public abstract class BaseTest {
 
     protected WebDriver driver;
+    private ChromeOptions options;
     private Properties properties;
 
     @BeforeClass(alwaysRun = true)
     public void setUpClass() {
         WebDriverManager.chromedriver().setup();
+        options = new ChromeOptions();
+        options.addArguments("--headless");
         properties = new Properties();
         try {
             properties.load(new FileInputStream(new File("src/test/resources/data.properties")));
@@ -32,7 +36,7 @@ public abstract class BaseTest {
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() {
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Long.parseLong(properties.getProperty("browser.implicitly.timeout.ms")), TimeUnit.MILLISECONDS);
         driver.manage().timeouts().pageLoadTimeout(Long.parseLong(properties.getProperty("browser.page.load.timeout.ms")), TimeUnit.MILLISECONDS);
         driver.manage().timeouts().setScriptTimeout(Long.parseLong(properties.getProperty("browser.set.script.timeout.ms")), TimeUnit.MILLISECONDS);
